2180834f43e074546d3ada4f2b56219433569dd3
Andrew Creyer <andall1811@aol.com>
2024-05-13 12:56:29 -0400

    World container testing

--- FallingObjectSpawner.gd
+++ FallingObjectSpawner.gd
@@ -5,8 +5,8 @@ var time_since_last_spawn = 0
 
 var falling_object_scene = preload("res://Scenes/TrashObject.tscn")
 
-func _process(delta):
-	time_since_last_spawn += delta
+func _process(_delta):
+	time_since_last_spawn += _delta
 
 	# Check if it's time to spawn a new object
 	if time_since_last_spawn >= SPAWN_INTERVAL:
@@ -16,8 +16,8 @@ func _process(delta):
 func spawn_object():
 	# Instantiate a new falling object and add it to the scene
 	var new_object = falling_object_scene.instantiate()
-	add_child(new_object)
 	new_object.position.x = randf_range(0, get_viewport().get_visible_rect().size.x)  # Randomize x position
-	new_object.position.y = -10  # Start above the screen.
-	var rotation_degrees = randf_range(0, 359)
-	new_object.rotation = rotation_degrees
+	new_object.position.y = -610  # Start above the screen.
+	var rot_degrees = randf_range(0, 359)
+	new_object.rotation = rot_degrees
+	get_parent().get_node("WorldContainer").add_child(new_object)
