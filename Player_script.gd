de45f9a3f08780bbe632cc91f99c083f1c6c22e8
Andrew Creyer <andall1811@aol.com>
2024-05-20 23:16:43 -0400

    Mergy stuff

--- Player_script.gd
+++ Player_script.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
+@export var SPEED = 150
 const JUMP_VELOCITY = -200
 var can_shoot = true
 @export var proj_scene : PackedScene
 @export var world: Node2D
 var planet_normal : Vector2
 var local_velocity : Vector2
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _ready():
 	wall_min_slide_angle = 0
@@ -25,15 +28,28 @@ func _physics_process(delta):
 	move_and_slide()
 
 func move_player(_delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		local_velocity.x = direction * SPEED
+	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
+	if horizontal_direction:
+		local_velocity.x = horizontal_direction * SPEED
+		sprite.flip_h = (horizontal_direction == -1)
 	else:
-		local_velocity.x = move_toward (local_velocity.x, 0, SPEED)
-	
+		local_velocity.x = move_toward(local_velocity.x, 0, SPEED)
+		
 	velocity = local_velocity.rotated(rotation)
 	if Input.is_action_pressed("jump") and is_on_floor():
 		local_velocity.y = JUMP_VELOCITY
+	update_animations(horizontal_direction)
+		
+func update_animations(horizontal_direction):
+	if is_on_floor():
+		if horizontal_direction == 0:
+			ap.play("Idle")
+		else:
+			if horizontal_direction != 0:
+				ap.play("Run")
+	else:
+		if velocity.y < 0:
+			ap.play("Jump")
 
 func check_shoot():
 	if Input.is_action_just_pressed("shoot") and can_shoot:
